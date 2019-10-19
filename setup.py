5d8f2c6cbb83c1311fd25aa144b595202b79c7a7
simon <simonm3@gmail.com>
2019-10-19 19:00:37 +0100

    version update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 params = dict(
     name='autogen',
     description='autogen',
-    version='0.0.5',
+    version='0.0.6',
     url='https://gitlab.com/simonm3/autogen.git',
     install_requires=['PyYAML', 'docopt', 'pipreqs',
                       "pywin32;platform_system=='Windows'"],
