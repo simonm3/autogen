2a8cbcd3719688659a4a6f0d0476da81effe706e
simon <simonm3@gmail.com>
2020-04-07 12:39:28 +0100

    version update

--- setup.py
+++ setup.py
@@ -7,30 +7,23 @@ overwritten when autogen is rerun.
 from setuptools import setup
 
 params = dict(
-    name="autogen",
-    description="Automate development tasks",
-    version="1.0.1",
-    url="https://gitlab.com/simonm3/autogen.git",
-    install_requires=["pipreqs", "pywin32;platform_system=='Windows'"],
-    packages=["autogen", "templates"],
-    package_data={
-        "templates": [
-            ".gitlab-ci.yml",
-            "example_image.jpg",
-            "example_page.rst",
-            "index.rst",
-            "Makefile",
-        ]
-    },
+    name='autogen',
+    description='Automate development tasks',
+    version='1.0.2',
+    url='https://gitlab.com/simonm3/autogen.git',
+    install_requires=['pipreqs', "pywin32;platform_system=='Windows'"],
+    packages=['autogen', 'templates'],
+    package_data={'templates': [
+        '.gitlab-ci.yml', 'example_image.jpg', 'example_page.rst', 'index.rst', 'Makefile']},
     include_package_data=True,
     py_modules=[],
-    scripts=None,
-)
+    scripts=None)
 
 ########## EDIT BELOW THIS LINE ONLY ##########
 
 # enable command line
-params.update(entry_points={"console_scripts": ["autogen = autogen.autogen:main"]})
+params.update(entry_points={"console_scripts": [
+              "autogen = autogen.autogen:main"]})
 
 ########## EDIT ABOVE THIS LINE ONLY ##########
 
